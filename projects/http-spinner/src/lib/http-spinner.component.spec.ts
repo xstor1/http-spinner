ddee98182d094a0be0b43d5c7b0a57e3bf8ac96b
Vojtěch Štor <vojtastor32@gmail.com>
2021-04-04 14:52:55 +0200

    angular cli core update

--- projects/http-spinner/src/lib/http-spinner.component.spec.ts
+++ projects/http-spinner/src/lib/http-spinner.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HttpSpinnerComponent } from './http-spinner.component';
 
@@ -6,7 +6,7 @@ describe('HttpSpinnerComponent', () => {
   let component: HttpSpinnerComponent;
   let fixture: ComponentFixture<HttpSpinnerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HttpSpinnerComponent ]
     })
